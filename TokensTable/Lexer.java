d24ef88dc26918fec924013f79d2bfe0cc3c786a
changoloco066 <bigoteschicos@gmail.com>
2026-04-21 14:45:12 -0600

    lexer class finnished and main class worked successfully

--- TokensTable/Lexer.java
+++ TokensTable/Lexer.java
@@ -3,55 +3,107 @@ import javax.swing.JFrame;
 import javax.swing.JTable;
 import java.util.*;
 
-public class Lexer{
+public class Lexer {
 
     private List<Tokens> tokens;
-    private static final Set <String> KEYWORDS = Set.of(
+
+    // 🔹 Diccionarios (base de clasificación)
+    private static final Set<String> KEYWORDS = Set.of(
         "if", "else", "while", "for", "int", "float", "return"
     );
 
-    private static final Set<Character> ARITHMETIC_OPS = Set.of(
-        '+', '-', '*', '/'
-    );
+    private static final Set<Character> ARITHMETIC_OPS = Set.of('+', '-', '*', '/');
+    private static final Set<Character> RELATIONAL_OPS = Set.of('>', '<', '=');
+    private static final Set<Character> PUNCTUATION = Set.of(';', ',', '(', ')', '{', '}');
 
-    private static final Set<Character> RELATIONAL_OPS = Set.of(
-        '>', '<', '='
-    );
-    private static final Set<Character> PUNCTUATION = Set.of(
-        ';', ',', '(', ')', '{', '}'
-    );
-
-    public Lexer(){
+    public Lexer() {
         tokens = new ArrayList<>();
-
     }
 
-    public List <Tokens> analyze(String input){
+    public List<Tokens> analyze(String input) {
         tokens.clear();
 
         String[] lines = input.split("\n");
 
-        for(int lineNum = 0; lineNum < lines.length; lineNum ++){
-            analyzeLine(lines [lineNum], lineNum + 1);
-
+        for (int lineNum = 0; lineNum < lines.length; lineNum++) {
+            analyzeLine(lines[lineNum], lineNum + 1);
         }
-        return tokens;
-    } 
 
-    private void analyzeLine(String line, int lineNumber){
+        return tokens;
+    }
+
+    private void analyzeLine(String line, int lineNumber) {
         int i = 0;
 
-        while (i < line.length()){
+        while (i < line.length()) {
             char c = line.charAt(i);
 
-            // condition to skip spaces
-            if (Character.isWhiteSpace(c)){
-                i ++; 
+            // Skip spaces
+            if (Character.isWhitespace(c)) {
+                i++;
                 continue;
             }
 
             int start = i;
-            
+
+            // 🔹 Identifiers / Keywords
+            if (Character.isLetter(c)) {
+                StringBuilder sb = new StringBuilder();
+
+                while (i < line.length() &&
+                       Character.isLetterOrDigit(line.charAt(i))) {
+                    sb.append(line.charAt(i));
+                    i++;
+                }
+
+                String word = sb.toString();
+
+                if (KEYWORDS.contains(word)) {
+                    addToken(word, TokenType.KEYWORD, start, lineNumber);
+                } else {
+                    addToken(word, TokenType.IDENTIFIER, start, lineNumber);
+                }
+            }
+
+            // 🔹 Numbers
+            else if (Character.isDigit(c)) {
+                StringBuilder sb = new StringBuilder();
+
+                while (i < line.length() &&
+                       Character.isDigit(line.charAt(i))) {
+                    sb.append(line.charAt(i));
+                    i++;
+                }
+
+                addToken(sb.toString(), TokenType.CONSTANT, start, lineNumber);
+            }
+
+            // 🔹 Operators
+            else if (ARITHMETIC_OPS.contains(c)) {
+                addToken(String.valueOf(c), TokenType.ARITHMETIC_OPERATOR, start, lineNumber);
+                i++;
+            }
+
+            else if (RELATIONAL_OPS.contains(c)) {
+                addToken(String.valueOf(c), TokenType.RELATIONAL_OPERATOR, start, lineNumber);
+                i++;
+            }
+
+            // 🔹 Punctuation
+            else if (PUNCTUATION.contains(c)) {
+                addToken(String.valueOf(c), TokenType.PUNCTUATION, start, lineNumber);
+                i++;
+            }
+
+            // 🔹 Unknown
+            else {
+                addToken(String.valueOf(c), TokenType.UNKNOWN, start, lineNumber);
+                i++;
+            }
         }
+    }
+
+    private void addToken(String lexeme, TokenType type, int pos, int line) {
+        tokens.add(new Tokens(lexeme, type, pos, line));
     }
 }
---
+++ TokensTable/main.java
@@ -0,0 +1,11 @@
+package TokensTable.TokensTable;
+
+public class main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            TokenTableGUI gui = new TokenTableGUI();
+            gui.setVisible(true);
+        });
+    }
+
+}
